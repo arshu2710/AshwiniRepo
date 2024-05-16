2f82d29bf7405f7bd79f207e2d29785cf30a6545
User <User@C_Name>
2024-05-16 12:08:40 +0530

    added page

---
+++ ComcatCRMGUIFramework/src/main/java/com/comcat/crm/objectrepositoryutility/Pagenew.java
@@ -0,0 +1,10 @@
+package com.comcat.crm.objectrepositoryutility;
+
+import org.testng.reporters.jq.Main;
+
+public class Pagenew {
+	public static void main(String[] args) {
+		System.out.println("hiii");
+	}
+
+}
